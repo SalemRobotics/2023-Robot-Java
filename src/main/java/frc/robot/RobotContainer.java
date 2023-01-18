1f8a7c390f4ed4e496bd7f3515f70fca084f4f1c
frc6324-team-laptop <81477144+frc6324-team-laptop@users.noreply.github.com>
2023-01-18 09:34:25 -0800

    file parsed continued

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import java.io.File;
 import java.nio.file.FileSystems;
 import java.util.Scanner;
 
@@ -31,12 +32,36 @@ public class RobotContainer {
    * @param controller The reference variable of the SparkMaxPIDController object.
    */
   public static void readPIDConstants(String objname, SparkMaxPIDController controller) {
-
-    double p, i, d;
-
     /* 
     TODO: finish file parsing; needs to read all the other constants, 
     and also the other constants need to be defined in the file. 
     */
+    try (Scanner scan = new Scanner(new File("constants/PIDConstants.json"))) {
+      double p=0, i=0, d=0;
+      while (scan.hasNextLine()) {
+        String nextLine = scan.nextLine();
+
+        // all numbers 0-9, characters "." and "-"
+        String strip = nextLine.replaceAll("[^0-9.-]", "");
+
+        // assign relevant constant values to respective double variables
+        if (nextLine.contains("p")) { 
+          p = Double.parseDouble(strip); 
+        }
+        else if (nextLine.contains("i")) { 
+          i = Double.parseDouble(strip); 
+        }
+        else if (nextLine.contains("d")) { 
+          d = Double.parseDouble(strip); 
+        }
+      }
+
+      // assign variables to PID controller configuration
+      controller.setP(p);
+      controller.setI(i);
+      controller.setD(d);
+    } catch (Exception e) {
+      DriverStation.reportError("File not found", true);
+    }
   }
 }
