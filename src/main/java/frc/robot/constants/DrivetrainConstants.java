009f508e8c985b8aefbcce00bb2a4fb9c387a6b1
frc6324-team-laptop <81477144+frc6324-team-laptop@users.noreply.github.com>
2023-01-17 17:42:29 -0800

    Separated constants into different files

---
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public class DrivetrainConstants {
+    public static final int kLeftFrontPort=0;
+    public static final int kLeftBackPort=1;
+    public static final int kLeftTopPort=2;
+
+    public static final int kRightFrontPort=3;
+    public static final int kRightBackPort=4;
+    public static final int kRightTopPort=3;  
+}
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,22 +6,22 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.constants.DrivetrainConstants;
 
 /**
  * Subsystem class for the drivetrain subsystem
  */
 public class Drivetrain extends SubsystemBase {
     MotorControllerGroup leftMotors = new MotorControllerGroup(
-        new CANSparkMax(Constants.DrivetrainConstants.kLeftFrontPort, MotorType.kBrushless),
-        new CANSparkMax(Constants.DrivetrainConstants.kLeftBackPort, MotorType.kBrushless),
-        new CANSparkMax(Constants.DrivetrainConstants.kLeftTopPort, MotorType.kBrushless)
+        new CANSparkMax(DrivetrainConstants.kLeftFrontPort, MotorType.kBrushless),
+        new CANSparkMax(DrivetrainConstants.kLeftBackPort, MotorType.kBrushless),
+        new CANSparkMax(DrivetrainConstants.kLeftTopPort, MotorType.kBrushless)
     );
 
     MotorControllerGroup rightMotors = new MotorControllerGroup(
-        new CANSparkMax(Constants.DrivetrainConstants.kRightFrontPort, MotorType.kBrushless),
-        new CANSparkMax(Constants.DrivetrainConstants.kRightBackPort, MotorType.kBrushless),
-        new CANSparkMax(Constants.DrivetrainConstants.kRightTopPort, MotorType.kBrushless)
+        new CANSparkMax(DrivetrainConstants.kRightFrontPort, MotorType.kBrushless),
+        new CANSparkMax(DrivetrainConstants.kRightBackPort, MotorType.kBrushless),
+        new CANSparkMax(DrivetrainConstants.kRightTopPort, MotorType.kBrushless)
     );
 
     DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
