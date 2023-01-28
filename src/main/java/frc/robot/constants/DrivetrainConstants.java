3df887a860cc5fab6e4462bda16c8bb8cebe14be
frc6324-team-laptop <81477144+frc6324-team-laptop@users.noreply.github.com>
2023-01-28 10:01:14 -0800

    NEO CAN ID's config'd

--- src/main/java/frc/robot/constants/DrivetrainConstants.java
+++ src/main/java/frc/robot/constants/DrivetrainConstants.java
@@ -2,11 +2,11 @@ package frc.robot.constants;
 
 public class DrivetrainConstants {
     // currently temporary
-    public static final int kLeftFrontPort=0;
-    public static final int kLeftBackPort=1;
-    public static final int kLeftTopPort=2;
+    public static final int kLeftFrontPort=1;
+    public static final int kLeftBackPort=2;
+    public static final int kLeftTopPort=3;
 
-    public static final int kRightFrontPort=3;
-    public static final int kRightBackPort=4;
-    public static final int kRightTopPort=3;  
+    public static final int kRightFrontPort=7;
+    public static final int kRightBackPort=8;
+    public static final int kRightTopPort=9;  
 }
