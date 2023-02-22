a5efd9a98bf31f23cf6abb4650fd515879ed3ac9
frc6324-team-laptop <81477144+frc6324-team-laptop@users.noreply.github.com>
2023-02-21 18:06:15 -0800

    Document autonomous branch

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix.sensors.PigeonIMU;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
-import com.revrobotics.CANSparkMax.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
@@ -23,6 +22,8 @@ import frc.robot.constants.DrivetrainConstants;
 
 /**
  * Subsystem class for the drivetrain subsystem.
+ * Allows tank drive control by a player-operated xbox controller.
+ * Contains methods for autonomous control, including external devices such as a gyroscope.
  */
 public class Drivetrain extends SubsystemBase {
     CANSparkMax leftFrontMotor = new CANSparkMax(DrivetrainConstants.kLeftFrontPort, MotorType.kBrushless);
@@ -43,7 +44,6 @@ public class Drivetrain extends SubsystemBase {
 
     PigeonIMU gyro = new PigeonIMU(DrivetrainConstants.kPidgeonPort);
 
-    // Create gyro and odometry
     DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(
         getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
 
@@ -68,6 +68,9 @@ public class Drivetrain extends SubsystemBase {
         updatePID();
     }
 
+    /**
+     * Updates Shuffleboard entries.
+     */
     void updateGyroShuffleboard() {
         /** Gyro **/
         SmartDashboard.putNumber("Yaw", gyro.getYaw());
@@ -76,11 +79,19 @@ public class Drivetrain extends SubsystemBase {
         SmartDashboard.putNumber("Roll", gyro.getPitch());
     }
 
+    /**
+     * Sends PID constants to Shuffleboard as editable fields. 
+     */
     void updatePID() {
         kP = SmartDashboard.getNumber("P", kP);
         kI = SmartDashboard.getNumber("I", kI);
     }
     
+    /**
+     * A command to drive the robot in whichever direction it needs to 
+     * in order to maintain a pitch of 0. This Is used to make sure the robot stays docked on the charger. 
+     * @return Runnable command.
+     */
     public CommandBase alignToCharger() {
         return run(
             () -> {
@@ -100,7 +111,7 @@ public class Drivetrain extends SubsystemBase {
 
 
     /** 
-     * sets arcade drive for motors
+     * Sets arcade drive for both sides of the drivetrain.
      * @param fwd Forward axis of drivetrain (Y axis)
      * @param rot Axis of rotation of drivetrain (X axis) 
      */
@@ -113,59 +124,92 @@ public class Drivetrain extends SubsystemBase {
     }
  
     /** 
-     * sets volts to control motors at a set speed 
+     * Sets the amount voltage for each side of the drivetrain. 
      */
     public void tankDriveVolts(double leftVolts, double rightVolts) {
-        rightFrontMotor.set(rightVolts);
-        leftFrontMotor.set(leftVolts);
+        rightFrontMotor.setVoltage(rightVolts);
+        leftFrontMotor.setVoltage(leftVolts);
         drive.feed();
     }
 
+    /**
+     * Get odometry position on the field
+     * @return Position (x, y), in meters
+     */
     public Pose2d getPose() {
         return odometry.getPoseMeters();
     }
 
+    /**
+     * Configure the scaling factor for using drive methods with motor controllers in a mode other than 
+     * PercentVbus or to limit the maximum output.
+     * The default value is 1.0.
+     * @param maxOutput Multiplied with the output percentage computed by the drive functions.
+     */
     public void setMaxOutput(double maxOutput) {
         drive.setMaxOutput(maxOutput);     
     }
 
+    /**
+     * Gets the current rotation of the robot.
+     * @return A {@linkplain Rotation2d} representing the rotation of the robot.
+     */
     public Rotation2d getRotation2d() {
         double yaw = gyro.getYaw();
         return Rotation2d.fromDegrees(Math.IEEEremainder(yaw, 360.0d));
     }
 
+    /**
+     * Gets the velocity of the left and right sides of the drivetrain. 
+     * @return A {@linkplain DifferentialDriveWheelSpeeds} object. 
+     */
     public DifferentialDriveWheelSpeeds getWheelSpeeds() {
         return new DifferentialDriveWheelSpeeds(leftEncoder.getVelocity(), rightEncoder.getVelocity());
     }
     
+    /**
+     * Gets the average of the distance travled by the left and right encoders
+     * @return Average distance, in meters
+     */
     public double getAverageEncoderDistance() {
         return (leftEncoder.getPosition() + rightEncoder.getPosition()) / 2.0;
     }
     
+    /**
+     * Resets encoders to 0.
+     */
     public void resetEncoders() {
         leftEncoder.setPosition(0);
         rightEncoder.setPosition(0);
     }
 
+    /**
+     * Resets the position of the robot on the field to (0,0) relative to it's current position
+     * @param pose Current position on the field
+     */
     public void resetOdometry(Pose2d pose) {
         resetEncoders();
         odometry.resetPosition(
             getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition(), pose);
     }
     /*
-     * uses the rotation to find how many degrees the bot is turned
+     * Pigeon gyro rotation in degrees.
      */
     public void getHeading() {
         getRotation2d().getDegrees();
     }
 
+    /**
+     * Gets the turn rate of the pigeon.
+     * @return
+     */
     public double getTurnRate() {
         double[] xyz = new double[3];
         gyro.getRawGyro(xyz);
         return xyz[1];
     }
     /*
-     * sets the yaw to 0.0
+     * Sets the yaw to 0.0
      */
     public void zeroHeading() {
         gyro.setYaw(0.0);
